22126cf84ecca0d36c49e2647f91857c8e554114
issamelferkh <issam.elferkh@gmail.com>
2019-11-22 17:37:41 +0000

    capture canvas from video -> OK

--- camagru/assets/js/main.js
+++ camagru/assets/js/main.js
@@ -1,71 +1,57 @@
-//const captureButton = document.querySelector("#capture-btn");
-// const video = document.querySelector('#video');
-
-// function getVideo(video) {
-//     navigator.mediaDevices
-//         .getUserMedia({
-//             video: true,
-//             audio: false
-//         })
-//         .then(stream => {
-//             video.srcObject = stream
-//             video.play()
-//         })
-//         .catch(err => console.error(`Oups !!`, err))
-// }
-// getVideo(video)
+// montage
 (function(){
-
-
-var video = document.getElementById('video'),
+    var video = document.getElementById('video'),
         canvas = document.getElementById('canvas'),
-        context = canvas.getContext("2d"),
-        photo = document.getElementById('photo'),
-        vendorURL = window.URL || window.webkitURL;
-    // navigator.getMedia = (navigator.getUserMedia ||
-    //     navigator.webkitGetUserMedia ||
-    //     navigator.mozGetUserMedia ||
-    //     navigator.msGetUserMedia);
-    // navigator.getMedia({
-    //     video: true,//{ width: 420, height: 320 },
-    //     audio: false
-    // }, function (stream) {
-    //     if ('srcObject' in video)
-    //         video.srcObject = stream;
-    //     else
-    //         video.src = vendorURL.createObjectURL(stream);
-    //     video.play();
-    // }, function (error) {
-    //     /*console.log("An error occured! " + error);*/
-    // });
+        context = canvas.getContext('2d'),
+        vendorUrl = window.URL || window.webkitURL;
+
     navigator.getUserMedia = navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia || navigator.msGetUserMedia;
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia || 
+                            navigator.msGetUserMedia;
 
-if (navigator.getUserMedia) {
-   navigator.getUserMedia({ audio: true, video: { width: 1280, height: 720 } },
-      function(stream) {
-         var video = document.querySelector('video');
-         video.srcObject = stream;
-         video.onloadedmetadata = function(e) {
-           video.play();
-         };
-      },
-      function(err) {
-         console.log("The following error occurred: " + err.name);
-      }
-   );
-} else {
-   console.log("getUserMedia not supported");
-}
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({audio: false, video: { width: 430, height: 320 }},
+            function(stream) {
+                var video = document.querySelector('video');
+                video.srcObject = stream;
+                video.onloadedmetadata = function(e) {
+                    video.play();
+                };
+            },
+            function(err) {
+                console.log("The following error occurred: " + err.name);
+            }
+        );
+
+        document.getElementById('capture').addEventListener('click', function() {
+            context.drawImage(video, 0, 0, 430, 320, 0, 0, 300, 150);
+        });
+    } else {
+        console.log("getUserMedia not supported");
+    }
 })();
-// // Capture the image, save it and then paste it to the DOM
-// captureButton.addEventListener("click", event => {
-// // Draw the image from the video player on the canvas
-// canvasElement.style.display = "block";
-// const context = canvasElement.getContext("2d");
-// context.drawImage(videoPlayer, 0, 0, canvas.width, canvas.height);
 
-// videoPlayer.srcObject.getVideoTracks().forEach(track => {
-// // track.stop();
-// });
+// (function() {
+//     var video = document.getElementById('video'),
+//         vendorUrl = window.URL || window.webkitURL;
+
+//     navigator.getMedia =    navigator.getUserMedia ||
+//                             navigator.webkitGetUserMedia ||
+//                             navigator.mozGetUserMedia || 
+//                             navigator.msGetUserMedia;
+
+//     navigator.getMedia({
+//         audio: false, 
+//         video: true
+//     }, function(stream) {
+//         video.src = vendorUrl.createObjectURL(stream);
+//         console.log(video.src);
+//         video.play();
+//     }, function(error) {
+//         console.log("The following error occured: " + error.name);
+//     });
+// })();
+
+
+
--- camagru/user/montage.php
+++ camagru/user/montage.php
@@ -13,110 +13,88 @@
         <div class="content" style="text-align: center;">
             <h2 class="content-subhead">Montage</h2><hr><br>
             <!-- filters            -->
-                <!-- <div class="pure-g">
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="../assets/img/filter01.png" alt="">
-                        <form class="pure-form">
-                            <button type="submit" class="pure-button">Choose filter</button>
-                        </form>
-                    </div>
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="../assets/img/filter02.png" alt="">
-                        <form class="pure-form">
-                            <button type="submit" class="pure-button">Choose filter</button>
-                        </form>
-                    </div>
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="../assets/img/filter03.png" alt="">
-                        <form class="pure-form">
-                            <button type="submit" class="pure-button">Choose filter</button>
-                        </form>
-                    </div>
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="../assets/img/filter04.png" alt="">
-                        <form class="pure-form">
-                            <button type="submit" class="pure-button">Choose filter</button>
-                        </form>
-                    </div>
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="../assets/img/filter05.png" alt="">
-                        <form class="pure-form">
-                            <button type="submit" class="pure-button">Choose filter</button>
-                        </form>
-                    </div>
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="../assets/img/filter06.png" alt="">
-                        <form class="pure-form">
-                            <button type="submit" class="pure-button">Choose filter</button>
-                        </form>
-                    </div>
-                </div> -->
-                <!-- video -->
-                <div class="montage-main">
-                    <div class="pure-u-1">
-                        <video class="montage-video" id="video"></video>
-                        <form class="pure-form">
-                            <button type="submit" class="pure-button" id="capture-btn">Capture</button>
-                        </form>
-                    </div>
-                    <br><br>
-                    <div class="pure-u-1">
-                        <!-- <img class="montage" src="../assets/img/1000.jpg" alt=""> -->
-                        <canvas class="montage-video" id="canvas"></canvas>
-                    </div>
+            <!-- <div class="pure-g">
+                <div class="pure-u-1-6">
+                    <img class="pure-img-responsive" src="../assets/img/filter01.png" alt="">
+                    <form class="pure-form">
+                        <button type="submit" class="pure-button">Choose filter</button>
+                    </form>
                 </div>
-                
-                <div class="montage-side">
-                    <div class="pure-g">
-                    <div class="pure-u-1-2">
-                            <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
-                        </div>
-                        <div class="pure-u-1-2">
-                            <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
-                        </div>
-                        <div class="pure-u-1-2">
-                            <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
-                        </div>
-                        <div class="pure-u-1-2">
-                            <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
-                        </div>
-                        <div class="pure-u-1-2">
-                            <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
-                        </div>
-                        <div class="pure-u-1-2">
-                            <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
-                        </div>
-                        <div class="pure-u-1-2">
-                            <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
-                        </div>
-                        <div class="pure-u-1-2">
-                            <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
-                        </div>
-                    </div>                
-                </div>                
-                <div class="montage-footer"></div>
-                <br><hr>
-                <!-- photo taken   -->
+                <div class="pure-u-1-6">
+                    <img class="pure-img-responsive" src="../assets/img/filter02.png" alt="">
+                    <form class="pure-form">
+                        <button type="submit" class="pure-button">Choose filter</button>
+                    </form>
+                </div>
+                <div class="pure-u-1-6">
+                    <img class="pure-img-responsive" src="../assets/img/filter03.png" alt="">
+                    <form class="pure-form">
+                        <button type="submit" class="pure-button">Choose filter</button>
+                    </form>
+                </div>
+                <div class="pure-u-1-6">
+                    <img class="pure-img-responsive" src="../assets/img/filter04.png" alt="">
+                    <form class="pure-form">
+                        <button type="submit" class="pure-button">Choose filter</button>
+                    </form>
+                </div>
+                <div class="pure-u-1-6">
+                    <img class="pure-img-responsive" src="../assets/img/filter05.png" alt="">
+                    <form class="pure-form">
+                        <button type="submit" class="pure-button">Choose filter</button>
+                    </form>
+                </div>
+                <div class="pure-u-1-6">
+                    <img class="pure-img-responsive" src="../assets/img/filter06.png" alt="">
+                    <form class="pure-form">
+                        <button type="submit" class="pure-button">Choose filter</button>
+                    </form>
+                </div>
+            </div> -->
+            <!-- video -->
+            <div class="montage-main">
+                <div class="pure-u-1">
+                    <video class="montage-video" id="video"></video><br>
+                    <a type="submit" class="pure-button" id="capture">Capture</a>
+                </div>
+                <br><br>
+                <div class="pure-u-1">
+                    <!-- <img class="montage" src="../assets/img/1000.jpg" alt=""> -->
+                    <canvas class="montage-video" id="canvas"></canvas>
+                </div>
+            </div>
+            <!-- photo taken   -->
+            <div class="montage-side">
                 <div class="pure-g">
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="https://images.unsplash.com/photo-1504884790557-80daa3a9e621?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="">
+                <div class="pure-u-1-2">
+                        <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
                     </div>
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="https://images.unsplash.com/photo-1542838686-37da4a9fd1b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80" alt="">
+                    <div class="pure-u-1-2">
+                        <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
                     </div>
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80" alt="">
+                    <div class="pure-u-1-2">
+                        <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
                     </div>
-                    <div class="pure-u-1-6">
-                    <img class="pure-img-responsive" src="https://images.unsplash.com/photo-1495366691023-cc4eadcc2d7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80" alt="">
-                    </div>                
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="https://images.unsplash.com/photo-1504279979626-c377a6c47600?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1825&q=80" alt="">
+                    <div class="pure-u-1-2">
+                        <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
                     </div>
-                    <div class="pure-u-1-6">
-                        <img class="pure-img-responsive" src="https://images.unsplash.com/photo-1547624643-3bf761b09502?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80" alt="">
+                    <div class="pure-u-1-2">
+                        <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
                     </div>
-                </div>
+                    <div class="pure-u-1-2">
+                        <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
+                    </div>
+                    <div class="pure-u-1-2">
+                        <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
+                    </div>
+                    <div class="pure-u-1-2">
+                        <img class="pure-img-responsive" src="../assets/img/1000.jpg" alt="">
+                    </div>
+                </div>                
+            </div>                
+            <div class="montage-footer"></div>
+            <br><hr>
+            </div>
         </div>
     </div>
 </div>
